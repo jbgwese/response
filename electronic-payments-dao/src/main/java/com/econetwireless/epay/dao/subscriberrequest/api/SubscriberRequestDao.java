d43c0221fe560d324089c4a5c910353b374c6b66
jbgwese <jbgwese@gmail.com>
2022-05-23 00:23:43 +0200

    bug fix #  : reverted the persist and update methods and updated to save for operability

--- electronic-payments-dao/src/main/java/com/econetwireless/epay/dao/subscriberrequest/api/SubscriberRequestDao.java
+++ electronic-payments-dao/src/main/java/com/econetwireless/epay/dao/subscriberrequest/api/SubscriberRequestDao.java
@@ -12,7 +12,4 @@ import java.util.List;
 public interface SubscriberRequestDao extends JpaRepository<SubscriberRequest, Long> {
     List<SubscriberRequest> findByPartnerCode(@Param("partnerCode") String partnerCode);
 
-    SubscriberRequest persist(SubscriberRequest subscriberRequest);
-
-    void update(SubscriberRequest createdSubscriberRequest);
 }
